b3d0c6d6d6d0edd8d903d614c3de057de803daa7
Haoran Wang <hwang827@gatech.edu>
2019-11-16 20:39:05 +0000

    unipacker

---
+++ yabin_2/header_accuracy.py
@@ -0,0 +1,13 @@
+from os import listdir
+
+# list of malware families to be classified as
+families = [f for f in listdir("/data/arsa/unpacked_binaries_unipacker")  if len([name for name in listdir("/data/arsa/unpacked_binaries_unipacker/"+f)])>=10]
+families = sorted(families, key=str.casefold)
+
+fd = open("accuracy.csv", 'w+')
+line = " "
+for family in families:
+    line = line + ", predicted_"+ family
+line = line+ ", no_predicted_family \n"
+fd.write(line)
+fd.close()
